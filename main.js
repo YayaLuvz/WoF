aaf972d13f9f73ee242c31876e62cd5a8fea95c8
th3guitarman <aaeronjhonsonwhyte@gmail.com>
2019-03-29 02:31:14 -0400

    lettersboard function - attempt to populate letters field. needs further formatting. wordDisplay.css to hold display

--- main.js
+++ main.js
@@ -20,7 +20,8 @@ var scoreboard = {
 }
 
 var solutions = {
-  fantasyBooks: ["The Lightning Thief", "The Sorcerer's Stone"]
+  currentIndex: 0,
+  fantasyBooks: ["The Lightning Thief", "The Sorcerer's Stone", "The Alchemyst"]
 };
 
 function letter(char) {
@@ -54,20 +55,20 @@ $('spin_button').click(function() {
   //}
   //pass_button enabled
   //  change class of button to disable repeated clicking
-}
+});
 //this function should be called when the wheel has spun and given its value
 function guessCons() {
   var guess = prompt('pick a consonant');
   if (letter(guess)=='consonant') {
     //check to see if the solution contains guess
-    if (/*solution contains guess*/) {
-      return true;
-    }
-         else {
-           return false;
-         }
+    //if (/*solution contains guess*/) {
+    //  return true;
+    //}
+    //     else {
+    //       return false;
+    //     }
   }
-
+}
 
 //VOWEL
 $('#vowel_button').click(function() {
@@ -77,46 +78,94 @@ $('#vowel_button').click(function() {
   //}
   //else {
   //  scoreboard.changeScore(p1, vowelValue * (guessVow() - 1));
-  //  populate letterboard
+  //  populate letter_board
   //}
   //pass_button enabled
   //change class of button to disable repeated clicking
 });
+
 function guessVow() {
   var guess = prompt('buy a vowel');
   if (letter(guess)=='vowel') {
-    if (/*solution contains guess*/) {
-      //populate letterboard
-      //get number of instances of guess in solution
-      return true /*number of instances of guess in solution */;
-    }
-    else {
-      return false;
-    }
+    // if (/*solution contains guess*/) {
+    //   //populate letterboard
+    //   //get number of instances of guess in solution
+    //   return true /*number of instances of guess in solution */;
+    // }
+    // else {
+    //   return false;
+    // }
   }
-
-
 }
 
 //SOLVE
 $('#solve_button').click(function(){
 
   //if (guessSolu()) {
-  //  you win function
+  //  you_win()
   //}
   //else {
   //  scoreboard.changeScore(p1, -2);
   //  disable other buttons
   //}
   //pass_button enabled
+  //change class of button to disable repeated clicking
 });
 
 function guessSolu() {
   var solve = prompt('guess the answer');
-  if (solve == /* solution */) {
-    return true;
-  }
-  else {
-    return false;
-  }
+  // if (solve == /* solution */) {
+  //   return true;
+  // }
+  // else {
+  //   return false;
+  // }
 }
+
+function div(parent, className) {
+  var r = document.createElement('div');
+  r.className = className;
+  parent.appendChild(r);
+  return r;
+}
+//function lettersxboard() {
+
+  var solution = 'solution solute';
+
+  function lettersboard() {
+    var letter_board = document.getElementById('letter_board'), letters;
+    letters = [];
+    while (letter_board.hasChildNodes()) {
+      letter_board.removeChild(letter_board.firstChild);
+    }
+
+    var word = div(letter_board, 'word');
+    for (var i = 0; i < solution.length; ++i) {
+      if (solution[i] == ' ') {
+        letters.push(div(letter_board, 'space'));
+        word = div(letter_board, 'word');
+      } else {
+        letters.push(div(word, 'letter'));
+      }
+      console.log(i, solution[i], word.children, letters.length);
+    }
+    //div(letter_board, 'clear');
+    // for (var i = 0; i < solution.length; ++i) {
+    //   letters[i].innerHTML = solution[i];
+    // }
+  }
+
+
+
+
+function chooseSolu() {
+  //cycle through object property array of solutions
+  if (solutions['currentIndex'] == solutions['fantasyBooks'].length) {
+    solutions['currentIndex'] = 0;
+  }
+
+  //index += 1;
+  return solutions['fantasyBooks'][solutions['currentIndex']++];
+
+  //}
+}//should be called when the game ends
