88091dc42a2056469688a7af15fb3adaba4adef0
th3guitarman <aaeronjhonsonwhyte@gmail.com>
2019-03-29 14:05:19 -0400

    guess_() mostly work. needs wheel, categories, can search, jquery works

--- js/main.js
+++ js/main.js
@@ -1,32 +1,34 @@
 var scoreboard = {
-  p1 : 0,
-  p2 : 0,
-  p3 : 0,
-  changeScore : function(pl, score) {
+  player_score : [5000, 0, 0],
+
+  changeScore(pl, score) {
     if (score == 12) {
-      this.pl = 0;
+      this.player_score[pl] = 0;
     }//bankrupt
     else if (score == -2) {
-      this.pl /= 2;
+      this.player_score[pl] /= 2;
     }//solve incorrectly
     else {
-      this.pl += score;
+      this.player_score[pl] += score;
     }
 
-    if (this.pl < 0)  {
-      this.pl = 0;
+    if (this.player_score[pl] < 0)  {
+      this.player_score[pl] = 0;
     }
   }//'pl' specifies which property gets changed, 'score' specifies by how much. + or -
 }
 
 var solutions = {
   currentIndex: 0,
-  fantasyBooks: ["The Lightning Thief", "The Sorcerer's Stone", "The Alchemyst"]
-};
+  fantasyBooks: ["THE LIGHTNING THIEF", "THE SORCERERS STONE", "THE ALCHEMYST"],
+  videoGames: ["HORIZON ZERO DAWN", "FINAL FANTASY XII"],
+  coolCharacters: ["LUCINA", "PROFESSOR OAK", "ONE PUNCH MAN"]
+}, solution;
 
-var letters = [];
 
-function letterType( char) {
+var letters = [], vowelValue = -250, vowelCount = 0;
+
+function letterType(char) {
     char = char.toUpperCase();
 
     switch(char) {
@@ -38,92 +40,142 @@ function letterType( char) {
         return 'vowel';
         break;
       default:
-        return 'consonant';
+        return 'not';
     }
 }
-function toggleWheel() {
 
+function toggleWheel() {
+  $('#overlay').toggle(1000);
 }
 
+function spinWheel() {
+  //startSpin();
+  return prize();
+}
+function prize() {
+  var pr = 300/*parseInt(indicatedSegment)*/;
+  return pr;
+}
 //SPIN
-$('spin_button').click(function() {
-  //showWheel();
-  //var spinValue = spinWheel(); -ideally returns spinvalue
-  //
-  //if (guessCons();) {
-  //  scoreboard.changeScore(p1, spinValue);
-  //
-  //}
-  //else {
-  //  disable vowel and solve buttons
-  //}
-  //pass_button enabled
-  //  change class of button to disable repeated clicking
+$("document").ready(function() {
+  $('#spin_button').click(function() {
+    toggleWheel();
+    var spinValue = spinWheel();
+    alert('spinning for ' + spinValue);
+    var cons = guessCons();
+    console.log(cons);
+    if (cons == true) {
+      scoreboard.changeScore(0, spinValue);
+      console.log(scoreboard.player_score[0]);
+    } else {
+    //  disable vowel and solve buttons
+      alert('sorry, bad guess');
+    }
+    toggleWheel();
+    //pass_button enabled
+    //  change class of button to disable repeated clicking
+  });
 });
+
 //this function should be called when the wheel has spun and given its value
 function guessCons() {
-  var guess = prompt('pick a consonant');
-  if (letterType(guess)=='consonant') {
+  var guess = prompt('pick a consonant').toUpperCase();
+  while (letterType(guess) == 'vowel') {
+    alert('entered a vowel. try again');
+    guess = prompt('pick a consonant').toUpperCase();
+  }
+
+  if (letterType(guess)!='vowel') {
     //check to see if the solution contains guess
-    //if (/*solution contains guess*/) {
-    //  return true;
-    //}
-    //     else {
-    //       return false;
-    //     }
+    if (searchSolu(solution, guess)) {
+      showLetter(solution, guess);
+      return true;
+    } else {
+      return false;
+    }
   }
 }
-
 //VOWEL
-$('#vowel_button').click(function() {
-  //scoreboard.changeScore(p1, vowelValue); //should be negative
-  //if (guessVow()==false) {
-  //  disable solve_button
-  //}
-  //else {
-  //  scoreboard.changeScore(p1, vowelValue * (guessVow() - 1));
-  //  populate letter_board
-  //}
+$('document').ready(function() {
+  $('#vowel_button').click(function() {
+    scoreboard.changeScore(0, vowelValue); //should be negative
+    alert('your score' + scoreboard.player_score[0]);
+    vowel = guessVow();
+    if (vowel == false) {
+      // disable solve
+    }
+    else {
+      scoreboard.changeScore(0, (vowelValue * (vowel - 1)));
+      alert('your score' + scoreboard.player_score[0]);
+    }
+
   //pass_button enabled
   //change class of button to disable repeated clicking
+  });
 });
-
 function guessVow() {
-  var guess = prompt('buy a vowel');
-  if (letterType(guess)=='vowel') {
-    // if (/*solution contains guess*/) {
-    //   //populate letterboard
-    //   //get number of instances of guess in solution
-    //   return true /*number of instances of guess in solution */;
-    // }
-    // else {
-    //   return false;
-    // }
+  var guess = prompt('buy a vowel').toUpperCase();
+  while (letterType(guess)!='vowel') {
+    alert('Not a vowel. try again');
+    guess = prompt('buy a vowel').toUpperCase();
+  }
+
+  if (searchSolu(solution, guess)) {
+    vowelCount = 0;
+    showLetter(solution, guess);
+    return vowelCount;
+  } else {
+    return false;
   }
 }
 
 //SOLVE
-$('#solve_button').click(function(){
+$('document').ready(function() {
+  var category = 1;
+  $('#solve_button').click(function(){
 
-  //if (guessSolu()) {
-  //  you_win()
-  //}
-  //else {
-  //  scoreboard.changeScore(p1, -2);
-  //  disable other buttons
-  //}
-  //pass_button enabled
-  //change class of button to disable repeated clicking
+    // if (guessSolu()) {
+    // //  you_win()
+    // }
+    // else
+    if (guessSolu() == false){
+      scoreboard.changeScore(0, -2);
+      alert('your score: '+ scoreboard.player_score[0]);
+    //  disable other buttons
+    }
+    else {
+      //youWin();
+      //change category
+      if (category = solutions.length) {
+        category = 0;
+      }
+      chooseSolu(category++);
+    }
+    //pass_button enabled
+    //change class of button to disable repeated clicking
+
+
+  });
 });
 
 function guessSolu() {
   var solve = prompt('guess the answer');
-  // if (solve == /* solution */) {
-  //   return true;
-  // }
-  // else {
-  //   return false;
-  // }
+  solve = solve.toUpperCase();
+  if (solve == solution) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+function searchSolu(str, substr) {
+  if (str.search(substr) > -1) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
 function div(parent, className) {
@@ -133,9 +185,7 @@ function div(parent, className) {
   return r;
 }
 
-var solution = 'solution solute';
-
-function lettersboard() {
+function lettersboard(str) {
   var letter_board = document.getElementById('letter_board');
 
   while (letter_board.hasChildNodes()) {
@@ -143,14 +193,14 @@ function lettersboard() {
   }
 
   var word = div(letter_board, 'word');
-  for (var i = 0; i < solution.length; ++i) {
-    if (solution[i] == ' ') {
+  for (var i = 0; i < str.length; ++i) {
+    if (str[i] == ' ') {
       letters.push(div(letter_board, 'space'));
       word = div(letter_board, 'word');
     } else {
       letters.push(div(word, 'letter'));
     }
-    console.log(i, solution[i], word.children, letters.length);
+    console.log(i, str[i], word.children, letters.length);
   }
   div(letter_board, 'clear');
   // for (var i = 0; i < solution.length; ++i) {
@@ -162,6 +212,9 @@ function showLetter(str, letter) {
   var indices = [];
   for(var i = 0; i < str.length; i++) {
     if (str[i] === letter){
+      if (letterType(letter) == 'vowel') {
+        vowelCount++;
+      }
       indices.push(i);
       letters[i].innerHTML = str[i];
     }
@@ -169,18 +222,17 @@ function showLetter(str, letter) {
   }
 }//gets the solution string and displays it in the boxes
 
-
-
-function chooseSolu() {
+//takes an int as parameter to choose the solution category
+function chooseSolu(cdex) {
   //cycle through object property array of solutions
-  if (solutions['currentIndex'] == solutions['fantasyBooks'].length) {
+  category = Object.keys(solutions)[cdex];
+
+  if (solutions['currentIndex'] == solutions[category].length) {
     solutions['currentIndex'] = 0;
   }
 
   //index += 1;
-  return solutions['fantasyBooks'][solutions['currentIndex']++];
-
-  //}
+  solution = solutions[category][solutions['currentIndex']++];
 }//should be called when the game ends
 
 //PASS
